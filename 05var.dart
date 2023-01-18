20ca8fb98bb0fc08b932f3852486f3bbc1920743
Aayush Basnet <122598100+Aayush-Basnet@users.noreply.github.com>
2023-01-18 16:41:26 +0545

    Declaration of Variable

---
+++ 05var.dart
@@ -0,0 +1,14 @@
+void main() {
+  var name = "Doom Devil";
+  var address = "Jhapa";
+  var service = [
+    "App Development",
+    "Software Development",
+    "DIgitral Marketing"
+  ];
+  var estyear = DateTime(2022);
+  print(estyear.year);
+  print(service);
+  print(name);
+  print(address);
+}
